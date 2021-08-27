ae4da6330960809aaeb28d0a63144e9d6dddcffc
robluizn <44649238+robluizn@users.noreply.github.com>
2021-08-27 10:55:37 -0300

    Add files via upload

---
+++ br.com.fulanodetal/src/br/com/fulanodetal/aula2/Exemplo.java
@@ -0,0 +1,8 @@
+package br.com.fulanodetal.aula2;
+
+public class Exemplo {
+	
+	int a=10;
+	double b=4.5;
+	String c="Roberto";
+}
---
+++ br.com.fulanodetal/src/br/com/fulanodetal/aula2/ExemploMain.java
@@ -0,0 +1,16 @@
+package br.com.fulanodetal.aula2;
+
+public class ExemploMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Exemplo e = new Exemplo();
+		
+		System.out.println(e.a);
+		System.out.println(e.b);
+		System.out.println(e.c);
+
+	}
+
+}
